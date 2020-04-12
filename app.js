816359179d372d412d0d15bd7513915d1438ff5a
Peter Shershov <peters@wix.com>
2020-04-13 01:56:37 +0300

    check victory of first row

--- app.js
+++ app.js
@@ -1,23 +1,61 @@
 const cells = document.getElementsByClassName("cell"); // [התאים שלנו]
 const players = document.getElementsByClassName("player"); // [השחקנים שלמעלה - איקס ועיגול]
 
-let turn = "X"; // התור הנוכחי 
+const firstRow = document.querySelector(".row"); // השורה הראשונה
+const firstRowCells = firstRow.getElementsByClassName("cell"); // התאים של השורה הראשונה
+
+let turn = "X"; // התור הנוכחי
 
 players[0].classList.add("selected"); // הצבע של הפלייר הראשון מתחלף לכתום
 
-for (const cell of cells) { // לופ על התאים
-  cell.addEventListener("click", () => { // מוסיף קליק איבנט על כל תא
-    if (cell.textContent === "") { // אם התא ריק
+function changeTurn() {
+  if (turn === "O") {
+    // אם התור הוא עיגול
+    turn = "X"; // התור הופך להיות איקס
+    players[1].classList.add("selected"); // הצבע של הפלייר השני מתחלף לכתום
+    players[0].classList.remove("selected"); // הצבע של הפלייר הראשון חוזר לאפור
+  } else if (turn === "X") {
+    // אחרת אם התור הוא איקס
+    turn = "O"; // התור הופך להיות עיגול
+    players[0].classList.add("selected"); // הצבע של הפלייר הראשון מתחלף לכתום
+    players[1].classList.remove("selected"); // הצבע של הפלייר השני מתחלף לאפור
+  }
+}
+
+function checkVictoryOfFirstRow() {
+  if (
+    firstRowCells[0].textContent === "X" &&
+    firstRowCells[1].textContent === "X" &&
+    firstRowCells[2].textContent === "X"
+  ) {
+    alert("Victory!!!!");
+  }
+}
+
+for (const cell of cells) {
+  // לופ על התאים
+  cell.addEventListener("click", () => {
+    // מוסיף קליק איבנט על כל תא
+    if (cell.textContent === "") {
+      // אם התא ריק
       cell.textContent = turn; // הטקסט של התא הוא התור
-      if (turn === "O") { // אם התור הוא עיגול
-        turn = "X"; // התור הופך להיות איקס
-        players[1].classList.add("selected"); // הצבע של הפלייר השני מתחלף לכתום
-        players[0].classList.remove("selected"); // הצבע של הפלייר הראשון חוזר לאפור
-      } else if (turn === "X") { // אחרת אם התור הוא איקס
-        turn = "O"; // התור הופך להיות עיגול
-        players[0].classList.add("selected"); // הצבע של הפלייר הראשון מתחלף לכתום
-        players[1].classList.remove("selected"); // הצבע של הפלייר השני מתחלף לאפור
-      }
+      changeTurn();
     }
+
+    checkVictoryOfFirstRow();
   });
 }
+
+/**
+ * שיעורי בית
+ * לבדוק את התאים בכל השורות
+ * האם יש בהם איקס
+ *
+ * אתה צריך לקבל את כל השורות
+ * להבין האם בתאים שלהם יש איקס
+ * ואם באחת מהן יש איקס בכל התאים
+ * לעשות אלרט
+ *
+ * loop
+ * if()
+ */
